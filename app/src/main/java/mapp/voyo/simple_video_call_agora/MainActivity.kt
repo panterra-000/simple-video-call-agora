d737e26f09aaf48b4141eb6bf5495bd707caae40
panterra-000 <raximjanovdavronbek@gmail.com>
2022-12-14 17:03:07 +0500

    full

--- app/src/main/java/mapp/voyo/simple_video_call_agora/MainActivity.kt
+++ app/src/main/java/mapp/voyo/simple_video_call_agora/MainActivity.kt
@@ -1,43 +1,199 @@
 package mapp.voyo.simple_video_call_agora
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.SurfaceView
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import mapp.voyo.simple_video_call_agora.ui.theme.SimplevideocallagoraTheme
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import io.agora.rtc2.*
+import io.agora.rtc2.video.VideoCanvas
+
 
 class MainActivity : ComponentActivity() {
+
+    // Fill the App ID of your project generated on Agora Console.
+    private val appId = "51e533886fc64afd8facac4a437f548c"
+
+    // Fill the channel name.
+    private val channelName = "FirstVideoCall"
+
+    // Fill the temp token generated on Agora Console.
+    private val token = "007eJxTYFCo/NqlvVy+VtV/37f3LKaqCRtfCEhemKFQfVrI6NODk+kKDKaGqabGxhYWZmnJZiaJaSkWaYnJickmiSbG5mmmJhbJH1fMTG4IZGR4vm82KyMDBIL4fAxumUXFJWGZKan5zok5OQwMALkTJKo="
+
+//    private val token ="007eJxTYIjYPidu1/PdLfdmmXur2VVfjiq48JnB+aXAgac3bz3TOyClwGBqmGpqbGxhYZaWbGaSmJZikZaYnJhskmhibJ5mamKRXL9yZnJDICODafVFRkYGCATx+RjcMouKS8IyU1LznRNzchgYAHnVJek="
+
+    private val appCertificate = "b513bc88c39441a89353082796e50cf6"
+
+    // An integer that identifies the local user.
+    private val uid = 0
+    private var isJoined = false
+
+    private var agoraEngine: RtcEngine? = null
+
+    //SurfaceView to render local video in a Container.
+    private var localSurfaceView: SurfaceView? = null
+
+    //SurfaceView to render Remote video in a Container.
+    private var remoteSurfaceView: SurfaceView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SimplevideocallagoraTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+        // If all the permissions are granted, initialize the RtcEngine object and join a channel.
+        if (!checkSelfPermission()) {
+            ActivityCompat.requestPermissions(this, REQUESTED_PERMISSIONS, PERMISSION_REQ_ID)
+        }
+        setupVideoSDKEngine()
+    }
+
+
+    private  val PERMISSION_REQ_ID = 22
+    private val REQUESTED_PERMISSIONS = arrayOf(
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.CAMERA
+    )
+
+    private fun checkSelfPermission(): Boolean {
+        return !(ContextCompat.checkSelfPermission(
+            this,
+            REQUESTED_PERMISSIONS[0]
+        ) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                    this,
+                    REQUESTED_PERMISSIONS[1]
+                ) != PackageManager.PERMISSION_GRANTED)
+    }
+
+    private val mRtcEventHandler: IRtcEngineEventHandler = object : IRtcEngineEventHandler() {
+        // Listen for the remote host joining the channel to get the uid of the host.
+        override fun onUserJoined(uid: Int, elapsed: Int) {
+            showMessage("Remote user joined $uid")
+
+            // Set the remote video view
+            runOnUiThread { setupRemoteVideo(uid) }
+        }
+
+        override fun onJoinChannelSuccess(channel: String, uid: Int, elapsed: Int) {
+            isJoined = true
+            showMessage("Joined Channel $channel")
+        }
+
+        override fun onUserOffline(uid: Int, reason: Int) {
+            showMessage("Remote user offline $uid $reason")
+            runOnUiThread { remoteSurfaceView!!.visibility = View.GONE }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SimplevideocallagoraTheme {
-        Greeting("Android")
+    private fun setupVideoSDKEngine() {
+        try {
+            val config = RtcEngineConfig()
+            config.mContext = baseContext
+            config.mAppId = appId
+            config.mEventHandler = mRtcEventHandler
+            agoraEngine = RtcEngine.create(config)
+            // By default, the video module is disabled, call enableVideo to enable it.
+            agoraEngine!!.enableVideo()
+        } catch (e: Exception) {
+            showMessage(e.toString())
+        }
     }
+
+    private fun setupRemoteVideo(uid: Int) {
+        val container = findViewById<FrameLayout>(R.id.remote_video_view_container)
+        remoteSurfaceView = SurfaceView(baseContext)
+        remoteSurfaceView!!.setZOrderMediaOverlay(true)
+        container.addView(remoteSurfaceView)
+        agoraEngine!!.setupRemoteVideo(
+            VideoCanvas(
+                remoteSurfaceView,
+                VideoCanvas.RENDER_MODE_FIT,
+                uid
+            )
+        )
+        // Display RemoteSurfaceView.
+        remoteSurfaceView!!.visibility = View.VISIBLE
+    }
+
+    private fun setupLocalVideo() {
+        val container = findViewById<FrameLayout>(R.id.local_video_view_container)
+        // Create a SurfaceView object and add it as a child to the FrameLayout.
+        localSurfaceView = SurfaceView(baseContext)
+        container.addView(localSurfaceView)
+        // Pass the SurfaceView object to Agora so that it renders the local video.
+        agoraEngine!!.setupLocalVideo(
+            VideoCanvas(
+                localSurfaceView,
+                VideoCanvas.RENDER_MODE_HIDDEN,
+                0
+            )
+        )
+    }
+
+    fun joinChannel(view: View?) {
+        if (checkSelfPermission()) {
+            val options = ChannelMediaOptions()
+
+            // For a Video call, set the channel profile as COMMUNICATION.
+            options.channelProfile = Constants.CHANNEL_PROFILE_COMMUNICATION
+            // Set the client role as BROADCASTER or AUDIENCE according to the scenario.
+            options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER
+            // Display LocalSurfaceView.
+            setupLocalVideo()
+            localSurfaceView!!.visibility = View.VISIBLE
+            // Start local preview.
+            agoraEngine!!.startPreview()
+            // Join the channel with a temp token.
+            // You need to specify the user ID yourself, and ensure that it is unique in the channel.
+            agoraEngine!!.joinChannel(token, channelName, uid, options)
+        } else {
+            Toast.makeText(applicationContext, "Permissions was not granted", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    fun leaveChannel(view: View?) {
+        if (!isJoined) {
+            showMessage("Join a channel first")
+        } else {
+            agoraEngine!!.leaveChannel()
+            showMessage("You left the channel")
+            // Stop remote video rendering.
+            if (remoteSurfaceView != null) remoteSurfaceView!!.visibility = View.GONE
+            // Stop local video rendering.
+            if (localSurfaceView != null) localSurfaceView!!.visibility = View.GONE
+            isJoined = false
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        agoraEngine!!.stopPreview()
+        agoraEngine!!.leaveChannel()
+
+        // Destroy the engine in a sub-thread to avoid congestion
+        Thread {
+            RtcEngine.destroy()
+            agoraEngine = null
+        }.start()
+    }
+
+
+    fun showMessage(message: String?) {
+        runOnUiThread {
+            Toast.makeText(
+                applicationContext,
+                message,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+
 }
